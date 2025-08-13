8bb2954244e2cd76ca24f11575bb6b2ba8fe99a5
Letícia Castro <letiscastros@gmail.com>
2025-08-12 21:18:18 -0300

    Create variaveis-entradas-dados.py

---
+++ exercicios.py/variaveis-entradas-dados.py
@@ -0,0 +1,12 @@
+#Exibição de frase com as populações de duas cidades e o total somado.
+
+populacaoNA = 45000 
+fraseNA = f"Nova Andradina tem a população de {populacaoNA}" 
+populacaoIV = 40000 
+popTotal = populacaoNA + populacaoIV 
+
+fraseTotal = f"Nova Andradina tem a população de {populacaoNA}, Ivinhema a população de {populacaoIV} e o total de {popTotal}" 
+
+print(fraseNA) 
+
+print(fraseTotal) 
